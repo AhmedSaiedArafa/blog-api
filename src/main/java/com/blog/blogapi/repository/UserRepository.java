fbe96b814b3929902a2b8e1f69423edec3c177d0
AhmedSaiedArafa <xbxahmedxbx@gmail.com>
2026-08-04 01:10:11 +0300

    Add User service layer

--- src/main/java/com/blog/blogapi/repository/UserRepository.java
+++ src/main/java/com/blog/blogapi/repository/UserRepository.java
@@ -4,5 +4,7 @@ import com.blog.blogapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    /* Repository layer responsible for database operations
+ JpaRepository provides ready-made methods like:save(), findAll(), findById(), delete(), etc. */ 
 
 }
---
+++ src/main/java/com/blog/blogapi/service/UserService.java
@@ -0,0 +1,39 @@
+package com.blog.blogapi.service;
+
+import com.blog.blogapi.entity.User;
+import com.blog.blogapi.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service   //Marks this class as a Service layer component managed by Spring
+public class UserService {
+
+// Dependency on UserRepository to communicate with the database
+    private final UserRepository userRepository;
+
+
+
+    // Constructor injection: Spring automatically provides UserRepository object
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+     
+    public List<User> getAllUsers() {
+        return userRepository.findAll(); 
+        /* Retrieves all users from the database
+         Calls JpaRepository's built-in findAll() method
+         Equivalent SQL: SELECT * FROM users;
+        */ 
+    }
+
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+        /*Creates a new user and saves it in the database
+         Calls JpaRepository's save() method
+         Equivalent SQL: INSERT INTO users VALUES (...);*/
+    }
+}
